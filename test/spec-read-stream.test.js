f938b7425782d33d0710d22d7b44bc018f180c89
MayamaTakeshi <mayamatakeshi@gmail.com>
2020-02-23 16:05:08 +0900

    replaced opts with just specs

--- test/spec-read-stream.test.js
+++ test/spec-read-stream.test.js
@@ -14,9 +14,7 @@ describe('spec-stream', () => {
 		})
 
 		it('should return single array if it is exact', () => {
-			var st = new SpecReadStream({
-				specs: [[10, 'abc']],
-			})
+			var st = new SpecReadStream([[10, 'abc']])
 
 			expect(st.read(10)).to.eql([[10, 'abc']])
 
@@ -24,9 +22,7 @@ describe('spec-stream', () => {
 		})
 
 		it('should return array with single array chunk if head of specs is larger than requested items', () => {
-			var st = new SpecReadStream({
-				specs: [[15, 'abc']],
-			})
+			var st = new SpecReadStream([[15, 'abc']])
 
 			expect(st.read(10)).to.eql([[10, 'abc']])
 
@@ -35,23 +31,12 @@ describe('spec-stream', () => {
 			expect(st.read(10)).to.be.null
 		})
 
-		it('should return array with default value if infinite is set', () => {
-			var st = new SpecReadStream({
-				infinite: true,
-				defaultValue: 0,
-			})
-
-			expect(st.read(10)).to.eql([[10, 0]])
-		})
-
 		it('should return array with multiple array chunks if first specs are smaller than requested items', () => {
-			var st = new SpecReadStream({
-				specs: [
-					[5, 'abc'],
-					[3, 'def'],
-					[2, 'ghi'],
-				]
-			})
+			var st = new SpecReadStream([
+				[5, 'abc'],
+				[3, 'def'],
+				[2, 'ghi'],
+			])
 
 			expect(st.read(10)).to.eql([[5, 'abc'], [3, 'def'], [2, 'ghi']])
 
@@ -59,33 +44,15 @@ describe('spec-stream', () => {
 		})
 
 		it('should return array with multiple array chunks if first specs are smaller than requested items', () => {
-			var st = new SpecReadStream({
-				specs: [
-					[5, 'abc'],
-					[3, 'def'],
-					[1, 'ghi'],
-				]
-			})
+			var st = new SpecReadStream([
+				[5, 'abc'],
+				[3, 'def'],
+				[1, 'ghi'],
+			])
 
 			expect(st.read(10)).to.eql([[5, 'abc'], [3, 'def'], [1, 'ghi']])
 
 			expect(st.read(10)).to.be.null
-		})
-
-		it('should return array with multiple array chunks if first specs are smaller than requested items', () => {
-			var st = new SpecReadStream({
-				specs: [
-					[5, 'abc'],
-					[3, 'def'],
-					[1, 'ghi'],
-				],
-				infinite: true,
-				defaultValue: 'default',
-			})
-
-			expect(st.read(10)).to.eql([[5, 'abc'], [3, 'def'], [1, 'ghi'], [1, 'default']])
-
-			expect(st.read(10)).to.eql([[10, 'default']])
 		})
 	})
 
